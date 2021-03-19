ddad0121a71f7be8de5279839d3926a1887358ba
iSanyok <supergulman09@gmail.com>
2021-03-19 14:08:27 +0300

    add price for 1 product in basket

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -9,18 +9,23 @@
                     alt=""
                     width="50" height="50"
                     style="margin-top: 15px">
-                <label style="margin-left: 25px">{{ $product->name }}</label>
-                <label style="margin-left: 25px">Цена: {{ $product->price * $count }}р</label>
-                <label style="margin-left: 25px">Количество: {{ $count }}</label>
-                <form method="POST" action="{{ route('add', $product) }}">
-                    @csrf
-                    <button style="flex: auto" class="btn btn-dark">+</button>
-                </form>
-                <form method="POST" action="{{ route('destroy', $product) }}">
-                    @csrf
-                    @method('DELETE')
-                    <button style="margin-top: 5px" class="btn btn-dark">-</button>
-                </form>
+                <label style="margin-left: 1em">{{ $product->name }}</label>
+                <label style="margin-left: 1em">Цена за 1шт. : {{ $product->price }}</label>
+                <label style="margin-left: 1em">Цена: {{ $product->price * $count }}р</label>
+                <label style="margin-left: 1em">Количество: {{ $count }}</label>
+                <div>
+                    <form method="POST" action="{{ route('destroy', $product) }}"
+                          style="float: left; padding-right: 1em">
+                        @csrf
+                        @method('DELETE')
+                        <button style="margin-top: 5px" class="btn btn-dark">-</button>
+                    </form>
+                    <form method="POST" action="{{ route('add', $product) }}"
+                    style="padding-top: 5px">
+                        @csrf
+                        <button style="flex: auto" class="btn btn-dark">+</button>
+                    </form>
+                </div>
             </div>
         @empty
             <label class="title" style="margin-left: 40%">Корзина пуста.</label>
