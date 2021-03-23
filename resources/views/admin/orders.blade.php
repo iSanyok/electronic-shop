0e44004c9bd9f27078e9a2b1131f4d5aa8667c32
iSanyok <supergulman09@gmail.com>
2021-03-23 16:04:03 +0300

    little refactor

--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -4,12 +4,11 @@
     <div class="container mb-3">
         <h2 class="text-center mb-2">ПРОСМОТР ЗАКАЗОВ</h2>
         @foreach($orders as $order)
-            @if($order->user)
                 <div class="mb-1">
-                    <label>Имя покупателя: {{ $order->user->name }}</label>
+                    <label>Имя покупателя: {{ $order->user->name ?? $order->customer->name }}</label>
                 </div>
                 <div class="mb-1">
-                    <label>Электронная почта: {{ $order->user->email }}</label>
+                    <label>Электронная почта: {{ $order->user->email ?? $order->customer->email }}</label>
                 </div>
                 <div class="mb-1">
                     <label>Цена заказа: {{ $order->price }}р.</label>
@@ -18,21 +17,6 @@
                     <a href="{{ route('order', ['order' => $order]) }}">
                         <button class="btn btn-secondary btn-sm mb-3">Перейти к заказу</button></a>
                 </div>
-            @else
-                <div class="mb-1">
-                    <label>Имя покупателя:{{ $order->customer->name }}</label>
-                </div>
-                <div class="mb-1">
-                    <label>Имя покупателя:{{ $order->customer->email }}</label>
-                </div>
-                <div class="mb-1">
-                    <label>Цена заказа: {{ $order->price }}р.</label>
-                </div>
-                <div class="mb-1">
-                    <a href="{{ route('order', ['order' => $order]) }}">
-                        <button class="btn btn-secondary btn-sm mb-2">Перейти к заказу</button></a>
-                </div>
-            @endif
         @endforeach
     </div>
     <div class="d-flex justify-content-center">
